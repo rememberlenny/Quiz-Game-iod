df3a0cd51020884c4027b4e17d479411d247189e
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-15 00:35:32 -0400

    9. Create function to initialize array content on view load

--- ATRQuizViewController.m
+++ ATRQuizViewController.m
@@ -18,6 +18,23 @@
 @end
 
 @implementation ATRQuizViewController
+    - (instancetype)initWithNibName:(NSString *):nibNameOrNil
+                    bundle:(NSString *):nibBundleOrNil
+    {
+        self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+        
+        if (self){
+            self.questions = @[@"From what is cognac made?",
+                               @"What is 7+7?",
+                               @"What is the capital of Vermont?"];
+            self.answer    = @[@"Grapes",
+                               @"14",
+                               @"Montipelier"];
+        }
+        
+        return self;
+    }
+
     - (IBAction)questionButton:(id)sender;
     - (IBAction)answerButton:(id)sender;
 @end
