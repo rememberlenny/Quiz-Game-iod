ca332935af107ce3365d80c4b53ff3b7d00598cc
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-15 00:00:30 -0400

    1. Create quiz view with xib

---
+++ ATRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  ATRQuizViewController.h
+//  ATRQuizGame
+//
+//  Created by Leonard Bogdonoff on 10/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ATRQuizViewController : UIViewController
+
+@end
